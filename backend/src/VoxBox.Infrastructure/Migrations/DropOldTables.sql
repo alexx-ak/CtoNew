54a94c95a10aa8a3e694c492a052138025e7cf2a
Aleksandar Kontrec <aleksandar.kontrec@factoryxhrvatska.onmicrosoft.com>
2026-01-14 17:46:27 +0100

    fix password, add appsettings.json, remove unneeded migration

--- backend/src/VoxBox.Infrastructure/Migrations/DropOldTables.sql
+++
@@ -1,4 +0,0 @@
--- Drop existing tables if they exist with integer-based schema
-IF OBJECT_ID('Tenants', 'U') IS NOT NULL DROP TABLE Tenants;
-IF OBJECT_ID('Users', 'U') IS NOT NULL DROP TABLE Users;
-GO
